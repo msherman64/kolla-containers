28c577a4153296365aaac6b06d1f0cd85d0fbba3
Michael Sherman <shermanm@uchicago.edu>
2023-08-07 13:53:26 -0500

    get CI working

--- do_build.sh
+++ do_build.sh
@@ -8,10 +8,13 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 rm -rf /tmp/kolla-tmpdir
 mkdir /tmp/kolla-tmpdir
 
-KOLLA_LOCAL_CHECKOUT=../kolla ./run.sh \
-    kolla-build \
+# KOLLA_LOCAL_CHECKOUT=../kolla \
+./run.sh kolla-build \
         --config-file kolla-build.conf \
         --template-override kolla-template-overrides.j2 \
         --work-dir /tmp/kolla-tmpdir \
+        --tag "xena-dc5823b" \
+        --cache \
         --profile neutron \
-        --profile blazar
+        --profile blazar \
+        --profile horizon
